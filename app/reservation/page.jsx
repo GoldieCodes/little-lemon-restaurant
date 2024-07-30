3761283e961017d8705f46137a4b535421d1f3e7
GoldieCodes <udeozoonyinye02@gmail.com>
2024-07-30 22:06:22 +0100

    made some style adjustments

--- app/reservation/page.jsx
+++ app/reservation/page.jsx
@@ -7,13 +7,13 @@ export default function Reservation() {
       <h1 className="border-b-2 border-yellow/45 text-2xl text-green">
         Table Reservation
       </h1>
-      <div className="min-h-[50vh] grid place-self-center align-middle">
-        <p className="text-xl text-[#9cab99] flex gap-6 items-center group">
-          You don't have any reservations.
+      <div className="min-h-[50vh] grid content-center">
+        <p className="text-[#9cab99] flex gap-4 items-center place-self-center">
+          <span className="text-2xl text-green/40">
+            <MdTableBar />
+          </span>
+          You haven't made any reservations.
         </p>
-        <span className="text-2xl text-green/40">
-          <MdTableBar />
-        </span>
         <Link href="/#" className="text-lg">
           Coming soon...
         </Link>
